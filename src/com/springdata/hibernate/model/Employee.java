72ada54c1f4709cfe905ceb88f9e24857eee5261
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-11 08:22:30 +0200

    adding Model Employee

---
+++ src/com/springdata/hibernate/model/Employee.java
@@ -0,0 +1,49 @@
+package com.springdata.hibernate.model;
+
+public class Employee {
+
+	private Long id;
+	
+	private String fullName;
+	
+	private int age;
+	
+	private String address;
+
+	public Employee() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
