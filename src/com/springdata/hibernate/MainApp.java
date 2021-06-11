7c9eb818ca6e2fcdf23237ac94767c5c61df6085
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-11 08:19:05 +0200

    adding hibernate.config.xml

---
+++ src/com/springdata/hibernate/MainApp.java
@@ -0,0 +1,33 @@
+package com.springdata.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+	
+
+
+
+		
+//		//Connect To DB
+//		String url = "jdbc:mysql://localhost:3306/employee";
+//		String username = "root";
+//		String password = "";
+//	
+//		try {
+//		
+//			Connection conn = DriverManager.getConnection(url, username, password);
+//			System.out.println("COnnected!!");
+//			System.out.println(conn);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+////			System.out.println(e.toString());
+//
+//		}
+		
+	}
+}
