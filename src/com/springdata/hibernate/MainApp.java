26e0a9dcef9c7bcf1aaed4eace1708a2e9ae632a
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-11 15:00:16 +0200

    get Employee By Id using criteriaQuery #5

--- src/com/springdata/hibernate/MainApp.java
+++ src/com/springdata/hibernate/MainApp.java
@@ -22,8 +22,7 @@ public class MainApp {
 		Session session = sessionFactory.getCurrentSession();
 		try {
 			session.beginTransaction();
-			List<Employee> emps = session.createQuery("from Employee emp where emp.age = 25"+
-			" AND emp.address = 'Egypt'").list();
+			List<Employee> emps = session.createQuery("from Employee emp where fullName like 'A%'").list();
 			for(int i=0; i<emps.size(); i++) {
 				System.out.println(emps.get(i).getFullName());
 			}
