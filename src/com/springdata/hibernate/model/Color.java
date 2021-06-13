022c92f3b2f478223e075069ca13c4e0a6426ab0
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-13 14:50:18 +0200

    add annotaion Car Color

--- src/com/springdata/hibernate/model/Color.java
+++ src/com/springdata/hibernate/model/Color.java
@@ -3,12 +3,35 @@ package com.springdata.hibernate.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clor")
 public class Color {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="name")
 	private String name;
 	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(
+			name="car_color",
+			joinColumns = @JoinColumn(name="color_id"),
+			inverseJoinColumns = @JoinColumn(name="car_id")
+			)
 	private List<Car> cars = new ArrayList<>();
 
 	public Color() {
